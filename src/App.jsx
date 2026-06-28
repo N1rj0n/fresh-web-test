c29e4332148bb286a828007b3ab3523fabf389a1
N1rj0n <96003343+N1rj0n@users.noreply.github.com>
2026-06-28 18:30:57 +0530

    Update App.jsx
    Change in Kalyan's Taste.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Kalyan Boudi Baaz</h1>
+          <h1>Kalyan Magi Baaz</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
